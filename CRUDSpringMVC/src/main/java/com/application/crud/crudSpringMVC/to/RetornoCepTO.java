b35c091a839dc12ca7e29f21a4ed90e034a43c36
neitonjc <78852153+neitonjc@users.noreply.github.com>
2021-03-18 16:09:12 -0300

    Add novos atributos
    Add novos atributos

---
+++ CRUDSpringMVC/src/main/java/com/application/crud/crudSpringMVC/to/RetornoCepTO.java
@@ -0,0 +1,10 @@
+package com.application.crud.crudSpringMVC.to;
+
+public class RetornoCepTO {
+	
+	public String cep;
+	public String logradouro;
+	public String bairro;
+	public String localidade;
+	public String uf;
+}
